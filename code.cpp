ddc60480d9ef5da70c620c736b30d8e49cd2d6ca
lefreire <letfreirefigueiredo@gmail.com>
2017-10-16 20:50:12 -0200

    Altering files path

--- code.cpp
+++ code.cpp
@@ -26,8 +26,8 @@ int main(){
 
     /*adding the file names*/
     vector<string> s;
-	s.push_back("0.json"); s.push_back("1.json"); s.push_back("2.json");
-	s.push_back("3.json"); s.push_back("4.json"); s.push_back("5.json"); 
+	s.push_back("events/0.json"); s.push_back("events/1.json"); s.push_back("events/2.json");
+	s.push_back("events/3.json"); s.push_back("events/4.json"); s.push_back("events/5.json"); 
 	for(int file = 0; file < s.size(); file++){
 		DataFile data;
 		cout << "Rodando codigo: prepareData()" << endl;
@@ -39,24 +39,24 @@ int main(){
 	 	data.prepareResults(s[file]);
 	 	// exit(0);
 
-		// Tracking teste;
-		// cout << "Rodando codigo: makeTracking()" << endl;
-		// teste.makeTracking(data);
+		Tracking teste;
+		cout << "Rodando codigo: makeTracking()" << endl;
+		teste.makeTracking(data);
 
-		// vector<TrackS> tracks = teste.getTracks();
-		// cout << "Rodando codigo: compareTracks(tracks)" << endl;
-		// data.compareTracks(tracks);
+		vector<TrackS> tracks = teste.getTracks();
+		cout << "Rodando codigo: compareTracks(tracks)" << endl;
+		data.compareTracks(tracks);
 
 
-		// ofstream trackFile("tracks.txt");
-		// for(int i = 0; i < tracks.size(); i++){
-		// 	vector<PrPixelHit> hits = tracks[i].getHits();
-		// 	trackFile << "(";
-		// 	for(int j = 0; j < hits.size(); j++){
-		// 		trackFile  << hits[j].id() << ", ";
-		// 	}
-		// 	trackFile << ")" << endl;
-		// }
+		ofstream trackFile("tracks.txt");
+		for(int i = 0; i < tracks.size(); i++){
+			vector<PrPixelHit> hits = tracks[i].getHits();
+			trackFile << "(";
+			for(int j = 0; j < hits.size(); j++){
+				trackFile  << hits[j].id() << ", ";
+			}
+			trackFile << ")" << endl;
+		}
 	}
 	//close the log file
 	log.close();
